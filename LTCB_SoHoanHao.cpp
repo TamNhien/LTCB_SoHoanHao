0616145ed72360c3e356c073a4da309dd52e307a
TamNhien <canhthien91@gmail.com>
2024-03-07 20:33:54 +0700

    Add project files.

---
+++ LTCB_SoHoanHao.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cout << "Nhap mot so nguyen duong n: "; cin >> n;
+
+    for (int i = 1; i < n; ++i)
+        if (n % i == 0) sum += i;
+
+    cout << (sum == n ? "La so hoan hao." : "Khong phai so hoan hao.") << endl;
+    return 0;
+}
+
